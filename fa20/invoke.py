13649d496af5abaf017a5f33999918c82c4beb5d
John M <j@ionlights.com>
2020-02-29 13:58:44 -0500

    adding fa20 for testing purposes

---
+++ fa20/invoke.py
@@ -0,0 +1,29 @@
+from pathlib import (
+    Path as _Path,
+)
+
+from tasks.concepts import (
+    Group as _Group,
+    Semester as _Semester,
+)
+
+syllabus = {}
+
+org_name = "ucfai"
+
+semester = _Semester(shortname="fa20")
+group = _Group("core", semester)
+
+paths = {}
+paths["disk"] = _Path(repr(group)) / repr(semester)
+paths["repo"] = f"https://github.com/{org_name}/{str(paths['disk'])}"
+paths["site"] = _Path("ucfai.org") / "content" / paths["disk"]
+
+hugo = {
+    "theme": "academic",
+}
+
+kaggle = {
+    "username": "ucfaibot",
+    "pulled": [],
+}
